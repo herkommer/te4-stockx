015a780fa868d3f7debd6e99d4afad0889f37e99
herkommer <m@herkommer.co.uk>
2019-12-12 16:49:08 +0100

    postman ready /anka and /anka/id

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-var Express = require('express');
-var MongoClient = require('mongodb');
-var BodyParser = require('body-parser');
+const Express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const BodyParser = require('body-parser');
+const uri = "mongodb+srv://dbuser:password667@acme-3ipgi.azure.mongodb.net/test?retryWrites=true&w=majority";
 
+var collection;
 var app = Express();
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended: true}));
 
-app.listen(4242, () => {});
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/anka', (req,res) => {
+    collection.find({}).toArray((err,result) => {
+        res.send(result);
+    })
+})
+
+app.get('/anka/:id', (req,res) => {
+    collection.findOne({'_id': new ObjectId(req.params.id)},(err,result) => {
+        res.send(result);
+    })
+})
+
+app.listen(4242, () => {
+
+    client.connect(err => {
+        collection = client.db("sample_supplies").collection("sales");
+        console.log('Connect to db');
+        //client.close();
+      });
+      
+
+});
